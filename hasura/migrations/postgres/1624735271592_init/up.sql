d8c31cca91db100636e38766f04592f1f5f0c658
Alex Harrison <alexjharrison@gmail.com>
2021-06-26 15:21:31 -0400

    update initial migration

--- hasura/migrations/postgres/1624735271592_init/up.sql
+++ hasura/migrations/postgres/1624735271592_init/up.sql
@@ -14,7 +14,6 @@ CREATE TABLE public.users (
     id text NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
-    username text NOT NULL,
     email text NOT NULL,
     display_name text NOT NULL
 );
@@ -22,7 +21,5 @@ ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_email_key UNIQUE (email);
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_username_key UNIQUE (username);
 CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_users_updated_at ON public.users IS 'trigger to set value of column "updated_at" to current timestamp on row update';
