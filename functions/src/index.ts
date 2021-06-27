2ac102e520864941a848ca0aa69b07a6da3ac2a0
Alex Harrison <alexjharrison@gmail.com>
2021-06-27 12:37:18 -0400

    allow myself admin access in cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,7 +16,8 @@ exports.processSignUp = functions.auth.user().onCreate((user) => {
   const customClaims = {
     "https://hasura.io/jwt/claims": {
       "x-hasura-default-role": "user",
-      "x-hasura-allowed-roles": ["public", "user", "admin"],
+      "x-hasura-allowed-roles":
+        user.email === "alexjharrison@gmail.com" ? ["user", "admin"] : ["user"],
       "x-hasura-user-id": user.uid,
     },
   };
