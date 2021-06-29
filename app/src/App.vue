92eb38edbc3c96f6ae0cf3655f659d8844658025
Alex Harrison <alexjharrison@gmail.com>
2021-06-29 08:03:01 -0400

    conditionally render page content

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <p>Hasura Console: <a href="/api/console" target="_blank">Here</a></p>
-  <p>Ngrok Console: <a href="http://localhost:4552" target="_blank">Here</a></p>
-  <p>Me: {{ user }}</p>
-  <Button v-if="auth.user" label="Sign Out" @click="signOut" />
-  <Button v-else @click="signIn">Sign In</Button>
+  <div v-if="!auth.isFetching">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <p>Hasura Console: <a href="/api/console" target="_blank">Here</a></p>
+    <p>
+      Ngrok Console: <a href="http://localhost:4552" target="_blank">Here</a>
+    </p>
+    <p>Me: {{ user }}</p>
+    <Button v-if="auth.user" label="Sign Out" @click="signOut" />
+    <Button v-else @click="signIn">Sign In</Button>
+  </div>
 </template>
 
 <script lang="ts">
